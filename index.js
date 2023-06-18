fc852f41781edac0946cff0872408a4fdd78343f
mohamed ali soussi <mohamedalisoussi227.gmailcom>
2023-06-18 21:51:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var items = document.getElementsByClassName("Item");
+var minBtn = document.getElementsByClassName("minus-btn");
+let plus = document.getElementsByClassName("plus-btn");
+var del = document.getElementsByClassName("delete");
+var price = document.getElementById("finalPrice");
+var lek = document.getElementsByClassName("like");
+//events
+for (let i = 0; i < plus.length; i++) {
+  plus[i].addEventListener("click", function () {
+    plus[i].previousElementSibling.value++;
+    total();
+  });
+}
+for (let i = 0; i < minBtn.length; i++) {
+  minBtn[i].addEventListener("click", function () {
+    if (minBtn[i].nextElementSibling.value > 1) {
+      minBtn[i].nextElementSibling.value--;
+      total();
+    }
+  });
+}
+for (let i = 0; i < lek.length; i++) { 
+  lek[i].addEventListener("click", function () {
+    if (lek[i].style.color == "red") {
+      lek[i].style.color = "black";
+    } else lek[i].style.color = "red";
+  });
+}
+for (let i = 0; i < del.length; i++) {
+  det[i].addEventListener("click", function () {
+    det[i].parentElement.remove();
+    total();
+  });
+}
+function total() {
+  let sum = 0;
+  for (let i = 0; i < items.length; i++) {
+    sum =
+      sum +
+      items[i].querySelector(".price").innerText *
+        items[i].querySelector(".Quant").value;
+  }
+  return (price.value = sum);
+}
+
